670ba1b1bdb30b468486e22bab3d917a0c43d21c
alaikal02 <alaikal.fajri@gmail.com>
2024-08-19 09:51:18 +0700

    feat(splash): menambahkan tampilan splash screen

---
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    // Splash screen akan ditampilkan selama 3 detik sebelum berpindah ke halaman berikutnya
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacementNamed(context, '/second');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.menu_book,
+              size: 100,
+              color: Colors.green,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Khataman Quran Al Barokah",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
